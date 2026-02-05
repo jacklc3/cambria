e5ff87c0d4b1cbc9f31b2b0be742743d9c3a8f0a
Jack Liell-Cock <jackliellcock@gmail.com>
2026-02-05 18:13:42 +0000

    making showType local

--- implementation/src/Inference/Types.hs
+++ implementation/src/Inference/Types.hs
@@ -46,19 +46,19 @@ data Arity = Arity {
 
 instance Show ValueType where
   show = showType 0
-
-showType :: Int -> ValueType -> String
-showType _ (TVar a) = a
-showType _ TUnit = "Unit"
-showType _ TInt = "Int"
-showType _ TBool = "Bool"
-showType _ TDouble = "Double"
-showType _ TString = "Str"
-showType _ TName = "Name"
-showType p (TPair t1 t2) = parensIf (p > 3) $ showType 4 t1 ++ " x " ++ showType 4 t2
-showType p (TEither t1 t2) = parensIf (p > 2) $ showType 3 t1 ++ " + " ++ showType 3 t2
-showType p (TFun t1 t2) = parensIf (p > 1) $ showType 2 t1 ++ " -> " ++ show t2
-showType p (THandler t1 t2) = parensIf (p > 0) $ show t1 ++ " => " ++ show t2
+    where
+      showType :: Int -> ValueType -> String
+      showType _ (TVar a) = a
+      showType _ TUnit = "Unit"
+      showType _ TInt = "Int"
+      showType _ TBool = "Bool"
+      showType _ TDouble = "Double"
+      showType _ TString = "Str"
+      showType _ TName = "Name"
+      showType p (TPair t1 t2) = parensIf (p > 3) $ showType 4 t1 ++ " x " ++ showType 4 t2
+      showType p (TEither t1 t2) = parensIf (p > 2) $ showType 3 t1 ++ " + " ++ showType 3 t2
+      showType p (TFun t1 t2) = parensIf (p > 1) $ showType 2 t1 ++ " -> " ++ show t2
+      showType p (THandler t1 t2) = parensIf (p > 0) $ show t1 ++ " => " ++ show t2
 
 parensIf :: Bool -> String -> String
 parensIf True s = "(" ++ s ++ ")"
